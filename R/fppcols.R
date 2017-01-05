a209a15d2b430a40054d81945bc536db46fe7462
robjhyndman <robjhyndman@gmail.com>
2017-01-05 14:57:23 +1100

    Modified fppcols order

--- R/fppcols.R
+++ R/fppcols.R
@@ -1,23 +1,23 @@
 # Colours for graphs in fpp2
 
 fppcols <- function(...)
-{  
+{
   fpp_palette <- function(n)
   {
     colvec <- c(
-    "#000000", #black
+    #Data="#000000", #black
     "#E69F00", #orange
     "#56B4E9", #sky_blue
+    "#CC79A7", #reddish_purple
+    "#D55E00", #vermillion
     "#009E73", #bluish_green
     "#F0E442", #yellow
-    "#0072B2", #blue
-    "#D55E00", #vermillion
-    "#CC79A7" #reddish_purple
+    "#0072B2" #blue
     )
     ncols <- length(colvec)
-    if (n > ncols) 
+    if (n > ncols)
     {
-      warning("This manual palette can handle a maximum of ", 
+      warning("This manual palette can handle a maximum of ",
         ncols, " values. You have supplied ", n, ".", call. = FALSE)
     }
     colvec[seq_len(n)]
