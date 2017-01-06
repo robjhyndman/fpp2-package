a7fdb9cce57ee0bccee8a19aaec1984d244b1886
Rob J Hyndman <robjhyndman@gmail.com>
2017-01-06 17:30:47 +1100

    Added alpha channel to fppcols()

--- R/fppcols.R
+++ R/fppcols.R
@@ -5,14 +5,14 @@ fppcols <- function(...)
   fpp_palette <- function(n)
   {
     colvec <- c(
-    "#E69F00", #orange
-    "#56B4E9", #sky_blue
-    "#CC79A7", #reddish_purple
-    "#009E73", #bluish_green
-    "#F0E442", #yellow
-    "#0072B2", #blue
-    "#D55E00", #vermillion
-    "#000000" #black
+    "#E69F0088", #orange
+    "#56B4E988", #sky_blue
+    "#CC79A788", #reddish_purple
+    "#009E7388", #bluish_green
+    "#F0E44288", #eyllow
+    "#0072B288", #blue
+    "#D55E0088", #vermillion
+    "#00000088" #black
     )
     ncols <- length(colvec)
     if (n > ncols)
