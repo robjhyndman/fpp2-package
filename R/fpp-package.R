cd02b803ddca8ac4bcfb5a3485bc2fea23a03afe
robjhyndman <robjhyndman@gmail.com>
2017-10-19 14:35:38 +1100

    Fixed spelling of corticosteroid

--- R/fpp-package.R
+++ R/fpp-package.R
@@ -407,7 +407,7 @@ NULL
 
 
 
-#' Monthly cortecosteroid drug sales in Australia from July 1991 to June 2008.
+#' Monthly corticosteroid drug sales in Australia from July 1991 to June 2008.
 #'
 #' Total monthly scripts for pharmaceutical products falling under ATC code
 #' H02, as recorded by the Australian Health Insurance Commission. Measured in
